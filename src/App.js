cb3da2d371e17f4c3822316b24e8a1e945439682
reza2boyce <reza2boyce@gmail.com>
2022-09-01 18:56:06 +0430

    reset everything

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import { Provider } from "react-redux";
-import store from './store';
-import Counter from './components/counter/Counter';
-import PostList from "./components/postList/PostList";
-
 const App = () => {
-  return (
-    <Provider store={store}>
-      <div className="App">
-        <Counter />
-        <PostList />
-      </div>
-    </Provider>
-  );
-};
+    return (
+        <div className="App">
+            <div>hello world</div>
+        </div>
+    )
+}
 
 export default App;
