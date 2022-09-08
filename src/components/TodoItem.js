57dffbb6eaa50ea6d59e588ab91fbbfd0b9bc78e
reza2boyce <reza2boyce@gmail.com>
2022-09-09 01:39:50 +0430

    finish checkbutton component

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,60 +2,91 @@ import { format } from "date-fns/esm";
 import toast from "react-hot-toast";
 import { MdDelete, MdEdit } from "react-icons/md";
 import { useDispatch } from "react-redux";
-import { deleteTodo } from "../slices/todoSlice";
+import { deleteTodo, updateTodo } from "../slices/todoSlice";
 import styles from "../styles/modules/todoItem.module.scss";
 import { getClasses } from "../utils/getClasses";
+import TodoModal from "./TodoModal";
+import { useState, useEffect } from "react";
+import CheckButton from "./CheckButton";
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
+  const [updateModalOpen, setUpdateModalOpen] = useState(false);
+
+  useEffect(() => {
+    if(todo.status === 'complete') {
+      setChecked(true);
+    } else {
+      setChecked(false);
+    }
+  }, [todo.status]);
+
   const handleDelete = () => {
     dispatch(deleteTodo(todo.id));
-    toast.success('Todo Deleted Successfully')
+    toast.success("Todo Deleted Successfully");
   };
 
   const handleUpdate = () => {
-    console.log("updating");
+    setUpdateModalOpen(true);
+    return;
   };
+  
+  const handleCheck = () => {
+    setChecked(!checked);
+    dispatch(updateTodo({
+      ...todo,
+      status: checked ? 'incomplete' : 'complete'
+    }))
+  }
 
   return (
-    <div className={styles.item}>
-      <div className={styles.todoDetails}>
-        []
-        <div className={styles.texts}>
-          <p
-            className={getClasses([
-              styles.todoText,
-              todo.status === "complete" && styles["todoText--completed"],
-            ])}
+    <>
+      <div className={styles.item}>
+        <div className={styles.todoDetails}>
+          <CheckButton checked={checked} handleCheck={handleCheck} />
+          <div className={styles.texts}>
+            <p
+              className={getClasses([
+                styles.todoText,
+                todo.status === "complete" && styles["todoText--completed"],
+              ])}
+            >
+              {todo.title}
+            </p>
+            <p className={styles.item}>
+              {format(new Date(todo.time), "p, MM/dd/yyyy")}
+            </p>
+          </div>
+        </div>
+        <div className={styles.todoActions}>
+          <div
+            className={styles.icon}
+            onClick={handleDelete}
+            onKeyDown={handleDelete}
+            role="button"
+            tabIndex={0}
           >
-            {todo.title}
-          </p>
-          <p className={styles.item}>
-            {format(new Date(todo.time), "p, MM/dd/yyyy")}
-          </p>
+            <MdDelete />
+          </div>
+          <div
+            className={styles.icon}
+            onClick={handleUpdate}
+            onKeyDown={handleUpdate}
+            role="button"
+            tabIndex={0}
+          >
+            <MdEdit />
+          </div>
         </div>
       </div>
-      <div className={styles.todoActions}>
-        <div
-          className={styles.icon}
-          onClick={handleDelete}
-          onKeyDown={handleDelete}
-          role="button"
-          tabIndex={0}
-        >
-          <MdDelete />
-        </div>
-        <div
-          className={styles.icon}
-          onClick={handleUpdate}
-          onKeyDown={handleUpdate}
-          role="button"
-          tabIndex={0}
-        >
-          <MdEdit />
-        </div>
-      </div>
-    </div>
+      <TodoModal
+        type='update'
+        todo={todo}
+        modalOpen={updateModalOpen}
+        setModalOpen={setUpdateModalOpen}
+      />
+    </>
   );
 };
 
--- src/components/TodoModal.js
+++ src/components/TodoModal.js
@@ -1,33 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "../styles/modules/modal.module.scss";
 import { MdOutlineClose } from "react-icons/md";
 import Button from "./Button";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../slices/todoSlice";
+import { addTodo, updateTodo } from "../slices/todoSlice";
 import { v4 as uuid } from "uuid";
 import toast from "react-hot-toast";
 
-const TodoModal = ({ modalOpen, setModalOpen }) => {
+const TodoModal = ({ type, modalOpen, setModalOpen, todo }) => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState("incomplete");
   const dispatch = useDispatch();
+  
+  useEffect(() => {
+    if(type === 'update' && todo) {
+      setTitle(todo.title);
+      setStatus(todo.status);
+    } else {
+      setTitle('');
+      setStatus('incomplete');
+    }
+  }, [type, todo, modalOpen]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (title && status) {
-      dispatch(
-        addTodo({
-          id: uuid(),
-          title,
-          status,
-          time: new Date().toLocaleString(),
-        })
-      );
-      toast.success('Task Added Successfully');
-      setModalOpen(false);
-    } else {
-      toast.error("Title shouldn't be empty");
+
+    if (title === "") {
+      toast.error("Please enter a title.");
     }
+
+    if (title && status) {
+      if (type === "add") {
+        dispatch(
+          addTodo({
+            id: uuid(),
+            title,
+            status,
+            time: new Date().toLocaleString(),
+          })
+        );
+        toast.success("Task Added Successfully");
+      }
+      if (type === "update") {
+        if (todo.title !== title || todo.status !== status) {
+          dispatch(
+            updateTodo({
+              ...todo,
+              title,
+              status,
+            })
+          );
+        } else {
+          toast.error('No Change Made');
+        }
+      }
+      setModalOpen(false);
+    } 
   };
 
   return (
@@ -44,7 +72,9 @@ const TodoModal = ({ modalOpen, setModalOpen }) => {
             <MdOutlineClose />
           </div>
           <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
-            <h1 className={styles.formTitle}>Add Task</h1>
+            <h1 className={styles.formTitle}>
+              {type === "update" ? "Update" : "Add"} Task
+            </h1>
             <label htmlFor="title">
               Title
               <input
@@ -68,7 +98,7 @@ const TodoModal = ({ modalOpen, setModalOpen }) => {
             </label>
             <div className={styles.buttonContainer}>
               <Button type="submit" variant="primary">
-                Add Task
+                {type === "update" ? "Update" : "Add"} Add Task
               </Button>
               <Button
                 type="button"
